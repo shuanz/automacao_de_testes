6b331930dfbfe1a4a8100a186fe4cc55b06759d1
shuanz <shuanz@gmail.com>
2024-11-08 17:23:17 -0300

    Configurar pipeline e adaptar testes para execução headless

---
+++ ferramentas/02_selenium_exercise/test_selenium.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def setup_driver():
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(options=chrome_options)
+    return driver
+
+# Resto do código do teste...
